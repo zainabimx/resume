10b8109ae4f8ea97854481fcd1ea55e778b0b9a6
zainabimx <153424664+zainabimx@users.noreply.github.com>
2024-12-19 22:55:17 +0530

    Add files via upload

---
+++ Resume-Bio-Website/script.js
@@ -0,0 +1,7 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
